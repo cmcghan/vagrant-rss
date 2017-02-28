52a0ae07fcf4cde4241edc6802c5e01f846f908e
Catharine McGhan <cmcghan@users.noreply.github.com>
2017-02-28 14:38:14 -0500

    Update get_rv_su_wd_f.sh

--- single_installers/get_rv_su_wd_f.sh
+++ single_installers/get_rv_su_wd_f.sh
@@ -32,10 +32,78 @@ echo "sets ROSVERSION, SCRIPTUSER, WORKSPACEDIR, FORCE environment variable for 
 # parse input vars (set to appropriate vars or default vars)
 #
 
+#
+# INPUT ARGUMENT PARSING:
+#
 
-
-
-
-
+# set defaults for input arguments
+ROSVERSION=
+SCRIPTUSER=vagrant
+WORKSPACEDIR="/home/$SCRIPTUSER/catkin_ws"
+FORCE=
+# if we get an input parameter (username) then use it, else use default 'vagrant'
+# get -f (force) if given -- NOTE: WILL -NOT- REMOVE OR FORCE-REINSTALL ROSARIA!!!
+if [ $# -lt 1 ]; then
+    echo "ERROR: No ROS version given as commandline argument. Exiting."
+    exit
+else # at least 1 (possibly 4) argument(s) at commandline...
+    # check against O/S argument, kinetic does not demand support for 14.04, or indigo/jade for 16.04...
+    echo "Commandline argument 1 is: $1"
+    if [ $1 == "indigo" ] && [ $UCODENAME == "trusty" ]; then
+        ROSVERSION="indigo"
+    elif [ $1 == "jade" ] && [ $UCODENAME == "trusty" ]; then
+        ROSVERSION="jade"
+    elif [ $1 == "kinetic" ] && [ $UCODENAME == "xenial" ]; then
+        ROSVERSION="kinetic"
+    else
+        echo "ERROR: Unknown ROS version given as commandline argument -or- ROS version does not match O/S."
+        echo "Currently, install_deps.sh supports trusty with indigo and jade only, xenial with kinetic only."
+        echo "Exiting."
+        exit
+    fi
+    echo "ROS version is $ROSVERSION."
+    if [ $# -lt 2 ]; then
+        echo "Single username not given as commandline argument. Using default of '$SCRIPTUSER'."
+    else # at least 2 (possibly more) arguments at commandline...
+        if [ "$2" == "-f" ]; then # -f is last argument at commandline...
+            echo "-f (force) commandline argument given."
+            FORCE=$2
+            echo "Default user and workspace directory path will be used."
+        else # SCRIPTUSER should be argument #2
+            # but we need to / should check against the users that have home directories / can log in
+            HOMEDIRFORUSER_FOUND=`ls -1 /home | grep -m 1 -o "$2" | wc -l`
+            # grep should find a match and repeat it
+            # and wc -l should give 1 if argument #2 is a username that has a home directory associated with it
+            if [ $HOMEDIRFORUSER_FOUND -eq 1 ]; then
+                echo "Username given as commandline argument."
+                SCRIPTUSER=$2
+                WORKSPACEDIR="/home/$SCRIPTUSER/catkin_ws"
+            else # already checked for a -f, and not a user... (note: WORKSPACEDIR not allowed to be given without SCRIPTUSER argument)
+                echo "Bad username given as commandline argument. Using default username."
+            fi
+            if [ $# -lt 3 ]; then
+                echo "Workspace not given as commandline argument. Using default of '$WORKSPACEDIR'."
+            else # at least 3 (possibly more) arguments at commandline...
+                if [ "$3" == "-f" ]; then # -f is last argument at commandline...
+                    echo "-f (force) commandline argument given."
+                    FORCE=$3
+                    echo "Default workspace directory path will be used."
+                else # WORKSPACEDIR should be argument #3
+                    echo "Workspace directory given as commandline argument."
+                    WORKSPACEDIR=$3
+                    if [ $# -gt 3 ] && [ "$4" == "-f" ]; then # at least 4 (possibly more) arguments at commandline...
+                        echo "-f (force) commandline argument given."
+                        FORCE=$4
+                    fi
+                fi
+            fi
+        fi
+    fi
+fi
+echo "Will be using user $SCRIPTUSER and directories at and under /home/$SCRIPTUSER..."
+echo "Will be setting up catkin workspace under $WORKSPACEDIR..."
+if [ "$FORCE" -eq "-f" ]; then
+    echo "Forcing install of all compiled-from-source components."
+fi
 
 echo "End of get_rv_su_wd_f.sh script!"
